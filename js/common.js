7d4e86576a8d6ba71c2f39091c435dabcda3fc12
김희정 <hjkim98@wisenut.co.kr>
2025-08-30 16:12:05 +0900

    feat 로그인 기능 구현

--- js/common.js
+++ js/common.js
@@ -20,3 +20,15 @@ navButtons.forEach((btn, index) => {
     }
   });
 });
+
+function showAlert(message) {
+  const modal = document.getElementById("alert-modal");
+  const msg = document.getElementById("alert-message");
+  msg.textContent = message;
+  modal.style.display = "flex";
+}
+
+function hideAlert() {
+  const modal = document.getElementById("alert-modal");
+  modal.style.display = "none";
+}
