f751955d6ae940087effb2abc6841385a7436c0e
hjk <ununsk@gmail.com>
2025-09-03 13:56:05 +0900

    fix 공통 fetch api 변경, form 호출 로직 분리

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
-function showAlert(message) {
+function showAlert(message, callbackFc = hideAlert) {
   const modal = document.getElementById("alert-modal");
   const msg = document.getElementById("alert-message");
+  const closeBtn = document.getElementById("alert-close");
   msg.textContent = message;
   modal.style.display = "flex";
+  closeBtn.onclick = callbackFc;
 }
 
 function hideAlert() {
@@ -23,50 +25,120 @@ async function apiFetch(url, optionsParam = {}) {
     ...optionsParam.headers,
     'Authorization': 'Bearer ' + token
   };
-  let data = optionsParam.data ?? null;
+  let body = optionsParam.body ?? null;
 
   // POST, PUT, DELETE → body와 Content-Type 추가
   let options = {
     method: method,
-    headers: headers
+    headers: headers,
+    body: body
   };
-  if (data && ['POST', 'PUT', 'DELETE'].includes(method)) {
-    headers['Content-Type'] = 'application/json; charset=UTF-8';
-    options.body = JSON.stringify(data);
-  }
-  
-  let res = await fetch(url, options);
-
-  // ❌ 토큰 만료
-  if (res.status === 401) {
-    const refresh = localStorage.getItem("refresh_token");
-    const refreshRes = await fetch("/refresh", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ refresh_token: refresh })
-    });
-
-    if (refreshRes.ok) {
-      const data = await refreshRes.json();
-      localStorage.setItem("access_token", data.access_token);
-
-      // ✅ 새로운 토큰으로 headers 업데이트
-      headers = {
-        ...headers,
-        Authorization: `Bearer ${data.access_token}`
-      };
-      options.headers = headers;
-
-      res = await fetch(url, options);
-      return await res.json();
-    } else {
-      // Refresh도 만료 → 로그인 화면으로 이동
-      localStorage.clear();
-      showAlert("세션 만료. 다시 로그인해주세요.");
-      window.location.href = "/login.html";
-      return;
+  if (body && ['POST', 'PUT', 'DELETE'].includes(method)) {
+    if (!headers['Content-Type']){
+      headers['Content-Type'] = 'application/json; charset=UTF-8';
     }
   }
+  
+  return fetch(url, options)
+    .then(async res => {
+      // 네트워크 오류 처리
+      // 서버에서 무조건 200을 던져주고, json으로 상태를 알려줌 
+      // 예시: { "code": 401, "message": "Not authenticated" }
+      const json = await res.json();
 
-  return await res.json();
+      if(json.code && json.code === 401) {
+        const refresh = localStorage.getItem("refresh_token");
+        const refreshRes = await fetch(`${ctx}/login/refresh`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ refresh_token: refresh })
+        });
+
+        if (refreshRes.ok) {
+          const data = await refreshRes.json();
+          localStorage.setItem("access_token", data.access_token);
+
+          // ✅ 새로운 토큰으로 headers 업데이트
+          headers = {
+            ...headers,
+            Authorization: `Bearer ${data.access_token}`
+          };
+          options.headers = headers;
+
+          res = await fetch(url, options);
+          return await res.json();
+        } else {
+          // Refresh도 만료 → 로그인 화면으로 이동
+          localStorage.clear();
+          showAlert("세션 만료. 다시 로그인해주세요.", () => {window.location.href = "/login.html";});
+          return;
+        }
+      }
+
+      return json;
+  });
+}
+
+async function formFetch(url, optionsParam = {}) {
+  const token = localStorage.getItem('access_token');
+  if (!token) {
+    location.href = './login.html';
+    return;
+  }
+
+  // 파라미터 세팅 (method, headers, data)
+  let method = optionsParam.method ?? 'GET';
+  let headers = {
+    ...optionsParam.headers,
+    'Authorization': 'Bearer ' + token
+  };
+  let body = optionsParam.body ?? null;
+
+  // POST, PUT, DELETE → body와 Content-Type 추가
+  let options = {
+    method: method,
+    headers: headers,
+    body: body
+  };
+  
+  return fetch(url, options)
+    .then(async res => {
+      // 네트워크 오류 처리
+      // 서버에서 무조건 200을 던져주고, json으로 상태를 알려줌 
+      // 예시: { "code": 401, "message": "Not authenticated" }
+      const json = await res.json();
+
+      if(json.code && json.code === 401) {
+        const refresh = localStorage.getItem("refresh_token");
+        const refreshFormData = new FormData();
+        refreshFormData.append("refresh_token", refresh);
+
+        const refreshRes = await fetch(`${ctx}/login/refresh`, {
+          method: "POST",
+          body: refreshFormData
+        });
+
+        if (refreshRes.ok) {
+          const data = await refreshRes.json();
+          localStorage.setItem("access_token", data.access_token);
+
+          // ✅ 새로운 토큰으로 headers 업데이트
+          headers = {
+            ...headers,
+            Authorization: `Bearer ${data.access_token}`
+          };
+          options.headers = headers;
+
+          res = await fetch(url, options);
+          return await res.json();
+        } else {
+          // Refresh도 만료 → 로그인 화면으로 이동
+          localStorage.clear();
+          showAlert("세션 만료. 다시 로그인해주세요.", () => {window.location.href = "/login.html";});
+          return;
+        }
+      }
+
+      return json;
+  });
 }
